b6fb91cdf936bfa73d3cbc9887c51261cb890f4a
死体 <50087831+xx-shitai-xx@users.noreply.github.com>
2023-01-19 19:47:07 +0100

    Update Program.cs

--- GS2Engine.TestApp/Program.cs
+++ GS2Engine.TestApp/Program.cs
@@ -5,14 +5,11 @@ using GS2Engine;
 using GS2Engine.GS2.Script;
 
 HashSet<Script> scripts = new();
-foreach (string file in Directory.GetFiles(
-	         $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}{Path.DirectorySeparatorChar}scripts"
-         ))
+foreach (string file in Directory.GetFiles($"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}{Path.DirectorySeparatorChar}scripts"))
 {
 	Console.WriteLine($"File: {file}");
 	scripts.Add(new(file, null, null, null));
 }
-
 
 while (true)
 {
@@ -20,15 +17,3 @@ while (true)
 
 	Thread.Sleep(10);
 }
-
-//await scripts.FirstOrDefault(x => x.Name.Equals("shared")).execute("encrypt").ConfigureAwait(false);
-
-/*
-Script script = new Script(File.ReadAllBytes("shared.gs2bc"));
-
-Console.WriteLine(JsonConvert.SerializeObject(script.Functions, Formatting.Indented));
-Console.WriteLine((await script.execute("onCreated"))?.Value);
-Stack<StackEntry> test = new();
-test.Push(new() {Type = StackEntryType.String, Value = "hello"});
-Console.WriteLine((await script.execute("encrypt", test))?.Value);
-*/

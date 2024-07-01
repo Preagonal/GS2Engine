de502a330c499685301226d397aed3a3a5647808
死体 <xx.shitai.xx@gmail.com>
2024-07-01 21:55:28 +0200

    Add missing object

--- GS2Engine.UnitTests/Objects/Drawing.cs
+++ GS2Engine.UnitTests/Objects/Drawing.cs
@@ -5,17 +5,8 @@ namespace GS2Engine.UnitTests.Objects;
 
 public class Drawing : VariableCollection
 {
-	private string? _image = "";
+	private string? _image;
 
-	/*
-	public Drawing ( string img, int x, int y ) : this ( img, x, y, null, null, null, null, null, null )
-	{
-	}
-
-	public Drawing ( string img, int x, int y, int cropx, int cropy, int width, int height ) : this ( img, x, y, cropx, cropy, width, height, null, null )
-	{
-	}
-	*/
 	public Drawing(
 		string? image,
 		int x,
@@ -29,15 +20,7 @@ public class Drawing : VariableCollection
 	)
 	{
 		_image   = image;
-		//Position = new(x, y);
-
-		//if (cropx != null && cropy != null && width != null && height != null)
-		//	Source = new(cropx.Value, cropy.Value, width.Value, height.Value);
-
 		Rotation = rotation;
-		//Scale    = scale;
-
-		//Game1.GetInstance ().SpriteBatch.Draw ( this._img, _position, _source, Color.White, rotation, new Vector2 ( 0, 0 ), scale, null, 0 );
 	}
 
 	public double Rotation
@@ -54,13 +37,4 @@ public class Drawing : VariableCollection
 
 	public void ChangeImgVis(ImgVis imgVis) => Layer = imgVis;
 
-	//public void ChangeImgPart(int x, int y, int w, int h) => Source = new Rectangle(x, y, w, h);
-}
-
-public enum ImgVis
-{
-	DrawUnderPlayer = 0,
-	LikeThePlayer   = 1,
-	DrawOverPlayer  = 2,
-	OnScreen        = 4,
 }
---
+++ GS2Engine.UnitTests/Objects/ImgVis.cs
@@ -0,0 +1,9 @@
+namespace GS2Engine.UnitTests.Objects;
+
+public enum ImgVis
+{
+	DrawUnderPlayer = 0,
+	LikeThePlayer   = 1,
+	DrawOverPlayer  = 2,
+	OnScreen        = 4,
+}
